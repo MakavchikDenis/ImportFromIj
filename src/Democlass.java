e680d39bf7b1352c83628d9501b6c5006cb43eca
MakavchikDenis <makavchik1.19@gmail.com>
2020-02-16 15:41:43 +0300

    Initial commit

---
+++ src/Democlass.java
@@ -0,0 +1,8 @@
+
+
+public class Democlass {
+    int var;
+    public Democlass( int var) {
+        this.var=var;
+    }
+}
---
+++ src/MainClass.java
@@ -0,0 +1,13 @@
+
+
+
+public class MainClass {
+    public static  void main (String args[]){
+        System.out.println(args[0]);
+        String name = "Denis";
+        System.out.println("name");
+        Democlass var=new Democlass(5);
+        System.out.println(var);
+        
+    }
+}
